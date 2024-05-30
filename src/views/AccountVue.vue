e5dc5859aa946b930afbf97d524d0ca76ff83278
stanislavberger <berger.pro@yandex.ru>
2024-05-30 16:33:14 +0300

    t01 inner page after Login

--- src/views/AccountVue.vue
+++ src/views/AccountVue.vue
@@ -1,41 +1,35 @@
 <script setup>
-import DataView from 'primevue/dataview';
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
+import Loader from '@/components/Loader.vue';
+import Card from 'primevue/card';
+
+const artworks = ref('');
+const showLoader = ref(false);
+
+const authStore = useAuthStore();
+
+const getAllArtworks = async () => {
+    showLoader.value = true;
+
+    try{
+        const response = await axios.get(`https://vue-jwt-e030f-default-rtdb.europe-west1.firebasedatabase.app/artists.json?auth=${authStore.userInfo.token}`)
+        artwork.value = response.data
+    } catch (err) {
+        console.error(err.response);
+    }
+}
+
+onMounted (async () => await getAllArtworks());
+
 </script>
 
 <template>
-    <DataView :value="products">
-    <template #list="slotProps">
-        <div class="grid grid-nogutter">
-            <div v-for="(item, index) in slotProps.items" :key="index" class="col-12">
-                <div class="flex flex-column sm:flex-row sm:align-items-center p-4 gap-3" :class="{ 'border-top-1 surface-border': index !== 0 }">
-                    <div class="md:w-10rem relative">
-                        <img class="block xl:block mx-auto border-round w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" />
-                        <Tag :value="item.inventoryStatus" :severity="getSeverity(item)" class="absolute" style="left: 4px; top: 4px"></Tag>
-                    </div>
-                    <div class="flex flex-column md:flex-row justify-content-between md:align-items-center flex-1 gap-4">
-                        <div class="flex flex-row md:flex-column justify-content-between align-items-start gap-2">
-                            <div>
-                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
-                                <div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
-                            </div>
-                            <div class="surface-100 p-1" style="border-radius: 30px">
-                                <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
-                                    <span class="text-900 font-medium text-sm">{{ item.rating }}</span>
-                                    <i class="pi pi-star-fill text-yellow-500"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex flex-column md:align-items-end gap-5">
-                            <span class="text-xl font-semibold text-900">${{ item.price }}</span>
-                            <div class="flex flex-row-reverse md:flex-row gap-2">
-                                <Button icon="pi pi-heart" outlined></Button>
-                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto md:flex-initial white-space-nowrap"></Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </template>
-</DataView>
+    <h2>Список картин</h2>
+    <Loader v-if="showLoader"/>
+   <Card v-for="(artwork, i) in artworks" :key="i">
+        <template #title>{{ artwork.name }}</template>
+    
+   </Card>
 </template>
